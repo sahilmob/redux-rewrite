a0b3acd8003307c11e21f89f8e38b220b23e7be9
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-24 18:47:54 +0300

    implement subscribe

--- redux.js
+++ redux.js
@@ -1,11 +1,21 @@
 function createStore(reducer) {
 	let state;
+	let listeners = [];
 
 	function getState() {
 		return state;
 	}
 
-	function subscribe(listener) {}
+	function subscribe(listener) {
+		listeners.push(listener);
+		return function unsubscribe() {
+			if (listeners.indexOf(listener) > -1) {
+				listeners.splice(listeners.indexOf(listener), 1);
+			} else {
+				throw new Error("Subscriber is not found");
+			}
+		};
+	}
 
 	function dispatch(action) {}
 
