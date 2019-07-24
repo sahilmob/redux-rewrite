580cd20d6bc41d7ec78ffa80b777304463b73644
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-24 18:43:49 +0300

    basic createStore setup

---
+++ redux.js
@@ -0,0 +1,13 @@
+function createStore(reducer) {
+	function getState() {}
+
+	function subscribe(listener) {}
+
+	function dispatch(action) {}
+
+	return {
+		getState,
+		subscribe,
+		dispatch
+	};
+}
